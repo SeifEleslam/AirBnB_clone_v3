e1bdb9a98ff208e8f8f38c292e86b2c0e7297aef
SeifEleslam <seifgouda98@gmail.com>
2024-03-31 13:43:58 +0200

    task10: places crud

---
+++ api/v1/views/places.py
@@ -0,0 +1,87 @@
+#!/usr/bin/python3
+"""
+Place Endpoints for the API
+"""
+
+from api.v1.views import app_views
+from models import storage
+from models.city import City
+from models.place import Place
+from models.user import User
+from flask import abort, request
+
+
+@app_views.route('/cities/<city_id>/places', methods=['GET'])
+def get_places(city_id):
+    """Return status OK for status route"""
+    city = storage.get(City, city_id)
+    if not city:
+        abort(404)
+    places = []
+    for place in city.places:
+        places.append(place.to_dict())
+    return places, 200
+
+
+@app_views.route('/places/<place_id>', methods=['GET'])
+def get_place(place_id):
+    """Return status OK for status route"""
+    place = storage.get(Place, place_id)
+    if not place:
+        abort(404)
+    return place.to_dict(), 200
+
+
+@app_views.route('/cities/<city_id>/places', methods=['POST'])
+def add_place(city_id):
+    """Return status OK for status route"""
+    city = storage.get(City, city_id)
+    if not city:
+        abort(404)
+    try:
+        body = request.get_json()
+    except Exception:
+        abort(400, "Not a JSON")
+    user_id = body.get('user_id')
+    if not user_id:
+        abort(400, "Missing user_id")
+    if not storage.get(User, user_id):
+        abort(404)
+    name = body.get('name')
+    if not name:
+        abort(400, "Missing name")
+    place = Place()
+    place.user_id = user_id
+    place.city_id = city_id
+    place.name = name
+    place.save()
+    return place.to_dict(), 201
+
+
+@app_views.route('/places/<place_id>', methods=['PUT'])
+def edit_place(place_id):
+    """Return status OK for status route"""
+    forbidden = ['id', 'created_at', 'updated_at', 'user_id', 'city_id']
+    place = storage.get(Place, place_id)
+    if not place:
+        abort(404)
+    try:
+        body = request.get_json()
+    except Exception:
+        abort(400, "Not a JSON")
+    for key, val in body.items():
+        if key not in forbidden:
+            setattr(place, key, val)
+    place.save()
+    return place.to_dict(), 200
+
+
+@app_views.route('/places/<place_id>', methods=['DELETE'])
+def del_place(place_id):
+    """Delete an existing place."""
+    place = storage.get(Place, place_id)
+    if not place:
+        abort(404)
+    storage.delete(place)
+    storage.save()
+    return {}, 200
