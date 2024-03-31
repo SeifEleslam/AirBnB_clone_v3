fb07ea01f733d8e9e3124461d3be20f7db8a5589
SeifEleslam <seifgouda98@gmail.com>
2024-03-31 08:16:41 +0200

    task9: users CRUD

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -18,58 +18,54 @@ def get_users():
     return users, 200
 
 
-# @app_views.route('/users/<user_id>', methods=['GET'])
-# def get_user(user_id):
-#     """Return status OK for status route"""
-#     user = storage.get(User, user_id)
-#     if not user:
-#         abort(404)
-#     return user.to_dict(), 200
+@app_views.route('/users/<user_id>', methods=['GET'])
+def get_user(user_id):
+    """Return status OK for status route"""
+    user = storage.get(User, user_id)
+    if not user:
+        abort(404)
+    return user.to_dict(), 200
 
 
-# @app_views.route('/users', methods=['POST'])
-# def add_user():
-#     """Return status OK for status route"""
-#     try:
-#         body = request.get_json()
-#     except Exception:
-#         abort(400, "Not a JSON")
-#     email = body.get('email')
-#     if not email:
-#         abort(400, "Missing email")
-#     password = body.get('password')
-#     if not password:
-#         abort(400, "Missing password")
-#     user = User()
-#     user.email = email
-#     user.password = password
-#     user.save()
-#     return user.to_dict(), 201
+@app_views.route('/users', methods=['POST'])
+def add_user():
+    """Return status OK for status route"""
+    try:
+        body = request.get_json()
+    except Exception:
+        abort(400, "Not a JSON")
+    name = body.get('name')
+    if not name:
+        abort(400, "Missing name")
+    user = User()
+    user.name = name
+    user.save()
+    return user.to_dict(), 201
 
 
-# @app_views.route('/users/<user_id>', methods=['PUT'])
-# def edit_user(user_id):
-#     """Return status OK for status route"""
-#     forbidden = ['id', 'created_at', 'updated_at', 'email']
-#     user = storage.get(User, user_id)
-#     if not user:
-#         abort(404)
-#     try:
-#         body = request.get_json()
-#     except Exception:
-#         abort(400, "Not a JSON")
-#     for key, val in body.items():
-#         if key not in forbidden:
-#             setattr(user, key, val)
-#     user.save()
-#     return user.to_dict(), 200
+@app_views.route('/users/<user_id>', methods=['PUT'])
+def edit_user(user_id):
+    """Return status OK for status route"""
+    forbidden = ['id', 'created_at', 'updated_at', 'email']
+    user = storage.get(User, user_id)
+    if not user:
+        abort(404)
+    try:
+        body = request.get_json()
+    except Exception:
+        abort(400, "Not a JSON")
+    for key, val in body.items():
+        if key not in forbidden:
+            setattr(user, key, val)
+    user.save()
+    return user.to_dict(), 200
 
 
-# @app_views.route('/users/<user_id>', methods=['DELETE'])
-# def del_user(user_id):
-#     user = storage.get(User, user_id)
-#     if not user:
-#         abort(404)
-#     storage.delete(user)
-#     storage.save()
-#     return {}, 200
+@app_views.route('/users/<user_id>', methods=['DELETE'])
+def del_user(user_id):
+    user = storage.get(User, user_id)
+    if not user:
+        abort(404)
+    storage.delete(user)
+    storage.save()
+    return {}, 200
