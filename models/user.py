c0e3892ff6446d34b5e6a14105af7c16ad7e2d54
SeifEleslam <seifgouda98@gmail.com>
2024-04-01 13:45:47 +0200

    secured_pwd

--- models/user.py
+++ models/user.py
@@ -6,6 +6,7 @@ from os import getenv
 import sqlalchemy
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+import hashlib
 
 
 class User(BaseModel, Base):
@@ -27,3 +28,6 @@ class User(BaseModel, Base):
     def __init__(self, *args, **kwargs):
         """initializes user"""
         super().__init__(*args, **kwargs)
+        secured = hashlib.md5()
+        secured.update(kwargs.get('password').encode('utf-8'))
+        self.password = secured.hexdigest()
